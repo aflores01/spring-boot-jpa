c3e10f37e81f7b1834b16a24d8b356db11efaa09
karunsane <karunsane@gmail.com>
2020-07-05 10:30:22 -0500

    Metodo DAO para guardar datos en BD, mapeo de vistas para generar formularios

--- src/main/java/com/learning/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/learning/springboot/app/controllers/ClienteController.java
@@ -1,5 +1,7 @@
 package com.learning.springboot.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.learning.springboot.app.models.dao.IClienteDao;
+import com.learning.springboot.app.models.entity.Cliente;
 
 @Controller
 public class ClienteController {
@@ -18,5 +21,19 @@ public class ClienteController {
 		model.addAttribute("titulo","Listado de clientes");
 		model.addAttribute("clientes",clienteDao.findAll());
 		return "listar";
+	}
+	
+	@RequestMapping("/form")
+	public String crear(Map<String,Object> model) {
+		Cliente cliente = new Cliente();
+		model.put("titulo", "Formulario cliente");
+		model.put("cliente", cliente);
+		return "form";
+	}
+	
+	@RequestMapping(value="/form", method=RequestMethod.POST)
+	public String guardar(Cliente cliente) {
+		clienteDao.save(cliente);
+		return "redirect:listar";
 	}
 }
