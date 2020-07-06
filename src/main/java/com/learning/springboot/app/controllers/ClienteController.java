0da83c3257530052c38312c58365fdaeef6b5693
karunsane <karunsane@gmail.com>
2020-07-05 19:15:50 -0500

    Edición de registros con validación en la persistencia

--- src/main/java/com/learning/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/learning/springboot/app/controllers/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,25 +20,39 @@ public class ClienteController {
 
 	@Autowired
 	private IClienteDao clienteDao;
-	@RequestMapping(value="/listar", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/listar", method = RequestMethod.GET)
 	public String listar(Model model) {
-		model.addAttribute("titulo","Listado de clientes");
-		model.addAttribute("clientes",clienteDao.findAll());
+		model.addAttribute("titulo", "Listado de clientes");
+		model.addAttribute("clientes", clienteDao.findAll());
 		return "listar";
 	}
-	
+
 	@RequestMapping("/form")
-	public String crear(Map<String,Object> model) {
+	public String crear(Map<String, Object> model) {
 		Cliente cliente = new Cliente();
 		model.put("titulo", "Formulario cliente");
 		model.put("cliente", cliente);
 		return "form";
 	}
-	
-	@RequestMapping(value="/form", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/form/{id}")
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model) {
+		Cliente cliente = null;
+		if (id != null) {
+			cliente = clienteDao.findOne(id);
+		} else {
+			return "redirect:/lsitar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo", "Editar Cliente");
+		return "form";
+	}
+
+	@RequestMapping(value = "/form", method = RequestMethod.POST)
 	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
 		if (result.hasErrors()) {
-			model.addAttribute("titulo","Listado de clientes");
+			model.addAttribute("titulo", "Listado de clientes");
 			return "form";
 		}
 		clienteDao.save(cliente);
--- src/main/java/com/learning/springboot/app/models/dao/ClienteDaoImpl.java
+++ src/main/java/com/learning/springboot/app/models/dao/ClienteDaoImpl.java
@@ -26,7 +26,17 @@ public class ClienteDaoImpl implements IClienteDao {
 	@Override
 	@Transactional
 	public void save(Cliente cliente) {
-		entityManager.persist(cliente);
+		if (cliente.getId() != null && cliente.getId() > 0) {
+			entityManager.merge(cliente);
+		} else {
+			entityManager.persist(cliente);
+		}
+	}
+
+	@Override
+	public Cliente findOne(Long id) {
+
+		return entityManager.find(Cliente.class, id);
 	}
 
 }
