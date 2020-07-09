7991713185eee137606526c52d1d028967ca42c0
karunsane <karunsane@gmail.com>
2020-07-09 13:09:08 -0500

    Inicialización de directorio

--- src/main/java/com/learning/springboot/app/models/service/UploadFileServiceImpl.java
+++ src/main/java/com/learning/springboot/app/models/service/UploadFileServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -52,6 +53,16 @@ public class UploadFileServiceImpl implements IUploadFileService {
 
 	public Path getPath(String filename) {
 		return Paths.get(UPLOADS_FOLDER).resolve(filename).toAbsolutePath();
+	}
+
+	@Override
+	public void deleteAll() {
+		FileSystemUtils.deleteRecursively(Paths.get(UPLOADS_FOLDER).toFile());
+	}
+
+	@Override
+	public void init() throws IOException {
+		Files.createDirectory(Paths.get(UPLOADS_FOLDER));
 	}
 
 }
